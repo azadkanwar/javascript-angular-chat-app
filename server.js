ce49272a9084892dbbd262f9e1cc78bded2187da
azad singh <azadsingh@azads-MacBook-Pro.local>
2020-06-01 19:03:02 +0530

    server path changed

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/javascript-angular-chat-app"));
+app.use(express.static(__dirname + "/dist/ui-kit-sample-app"));
 app.get("/*", function (req, res) {
-  res.sendFile(
-    path.join(__dirname + "/dist/javascript-angular-chat-app/index.html")
-  );
+  res.sendFile(path.join(__dirname + "/dist/ui-kit-sample-app/index.html"));
 });
 app.listen(process.env.PORT || 8080);
