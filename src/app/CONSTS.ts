2090f16542a33c0b6c3140975efb1a62324c19dd
azad singh <azadsingh@azads-MacBook-Pro.local>
2020-06-01 15:57:56 +0530

    api key

--- src/app/CONSTS.ts
+++ src/app/CONSTS.ts
@@ -1,7 +1,7 @@
 export const COMETCHAT_CONSTANTS = {
-    APP_ID: '9992e0c46c915d',
-    REGION: 'US',
-    API_KEY: '606028d3c3ecff1b2014886376054ada6621ef97'
-}
+  APP_ID: "195662737397333",
+  REGION: "US",
+  API_KEY: "4ba8128ba16e0893fa458d4ad7cff805af26f3f5",
+};
 
 // http://localhost:4200/?uid=ajaygajra&apiKey=606028d3c3ecff1b2014886376054ada6621ef97&appId=9992e0c46c915d&region=us
