95de493c026e6d3d772cee8b6d291dd79f818378
azad singh <azadsingh@azads-MBP.domain.name>
2020-06-01 16:34:24 +0530

    path name and app name chnages

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/cometchat-video"));
+app.use(express.static(__dirname + "/dist/javascript-angular-chat-app"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/cometchat-video/index.html"));
+  res.sendFile(
+    path.join(__dirname + "/dist/javascript-angular-chat-app/index.html")
+  );
 });
 app.listen(process.env.PORT || 8080);
